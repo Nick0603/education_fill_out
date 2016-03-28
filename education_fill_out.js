2ba41e3d93873c43a481cb6305e4ae821d5d0ed9
Nick <b10303008@gmail.com>
2016-03-28 21:33:57 +0800

    更新業界師資選項未選到 bug (id不同)

--- education_fill_out.js
+++ education_fill_out.js
@@ -1,17 +1,36 @@
+//產生指定整數亂數
 function GetRandom(Min,Max){
   return Math.floor(Math.random()* (Max-Min+1) )+Min
 }
 
-var text = 0;
-var n = 0;
-while(text != null){
-	n++;
-	text = document.getElementById('_1_' + n + '_1');
+//計算有多少個欄位
+function count_id( count , index){
+	var test = 0;
+	while(true){
+		test = document.getElementById('_' + index + '_' + count + '_1');
+		if(test == null){
+			break
+		}
+		count++;
+	}
+	return (count-1);
 }
-n--;
 
-for(var i=1;i<=n;i++){
-	text = document.getElementById('_1_' + i + '_' + GetRandom(2,4));
-	text.click();
+//填寫兩個部分一般、業界師資、亂數選項(最小1最大5)
+function fill_out( n , m , Min , Max){
+	var test = 0;
+	for(var i=1;i<=n;i++){
+	test = document.getElementById('_1_' + i + '_' + GetRandom( Min , Max));
+	test.click();
+	}
+	for(;i<=m;i++){
+	test = document.getElementById('_5_' + i + '_' + GetRandom( Min , Max));
+	test.click();
+	}
 }
-Button1.click();
+
+
+var n = count_id(1,1); //一般選項欄位數
+var m = count_id(n+1,5); //業界選項欄位數
+fill_out(n,m,2,4);//填寫表單
+Button1.click();//送出
